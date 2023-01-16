d5319a4e7b2424bb5dea51daa2850e5786d45ddf
Jessica Ejelöv <jessicaejelov@gmail.com>
2023-01-16 23:17:32 +0100

    global context removed: could not get it to update after POST

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import Navigation from "./layouts/navbar";
 import About from "./components/about";
 import Categorieslist from "./components/categorieslist";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//import context and wrapp app content in it to use it and state thru the app
-import { MoneyTrackerProvider } from "./context/context";
 
 //create the basic layout
 function App() {
   return (
     <>
-        <MoneyTrackerProvider>
           <Router>
             <Navigation />
             <main>
@@ -20,7 +17,6 @@ function App() {
               </Routes>
             </main>
           </Router>
-        </MoneyTrackerProvider>
     </>
   );
 }
